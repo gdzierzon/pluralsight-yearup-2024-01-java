6fe66f5081e63e318db685ec22ec34c1dc570fd1
Gregor Dzierzon <gregor@fullstackdevs.net>
2024-02-01 15:43:22 -0700

    added loops arrays final

---
+++ week 1/demos/04_loops_arrays_demo_final/src/com/pluralsight/CommonInterviewQuestions.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class CommonInterviewQuestions
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    // input array of number
+    // add all even numbers
+    // return the result
+}
+
