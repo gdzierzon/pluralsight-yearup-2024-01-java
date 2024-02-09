8e4ee328500bd427e0a2dde9d193aeab84bc5c51
Gregor Dzierzon <gregor@fullstackdevs.net>
2024-02-09 15:23:15 -0700

    added order inheritance example

---
+++ week 2/DzierzonsPizzeria_MVC_Orders_Inheritance/src/main/java/com/pluralsight/models/DineInOrder.java
@@ -0,0 +1,54 @@
+package com.pluralsight.models;
+
+public class DineInOrder extends Order
+{
+    private String reservationTime;
+    private int numberOfGuests;
+    private int table;
+    private String server;
+
+    public DineInOrder(String name)
+    {
+        super(name);
+    }
+
+    public String getReservationTime()
+    {
+        return reservationTime;
+    }
+
+    public void setReservationTime(String reservationTime)
+    {
+        this.reservationTime = reservationTime;
+    }
+
+    public int getNumberOfGuests()
+    {
+        return numberOfGuests;
+    }
+
+    public void setNumberOfGuests(int numberOfGuests)
+    {
+        this.numberOfGuests = numberOfGuests;
+    }
+
+    public int getTable()
+    {
+        return table;
+    }
+
+    public void setTable(int table)
+    {
+        this.table = table;
+    }
+
+    public String getServer()
+    {
+        return server;
+    }
+
+    public void setServer(String server)
+    {
+        this.server = server;
+    }
+}
